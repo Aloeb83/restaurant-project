36b22c2cd8051d61797ed7aac9adb407241cd82f
Alec Loeber <alecloeber@yahoo.com>
2019-01-09 17:17:29 -0500

    Made sub-menus visable from frontend.

--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Menu
 
 # Create your views here.
+
+def home_page(request):
+    menus = Menu.objects.filter()
+    stuff_for_frontend = {'menus': menus}
+    return render(request, 'menu/home_page.html', stuff_for_frontend)

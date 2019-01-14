eedd6285db87a609612d2683ad1f14b499970bc5
Alec Loeber <alecloeber@yahoo.com>
2019-01-14 14:06:49 -0500

    Added ability to make items and show them on the sub-menu modal.

--- menu/admin.py
+++ menu/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Menu
+from .models import Menu, Item
 
 # Register your models here.
 
 admin.site.register(Menu)
+admin.site.register(Item)
--- menu/models.py
+++ menu/models.py
@@ -10,3 +10,13 @@ class Menu(models.Model):
     
     def __str__(self):
         return self.name
+
+class Item(models.Model):
+    menu = models.ForeignKey('menu.Menu', on_delete=models.CASCADE, related_name='item')
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=3, decimal_places=1)
+
+    def __str__(self):
+        return self.name
+

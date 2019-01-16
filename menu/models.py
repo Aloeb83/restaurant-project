2786660c607f7dfcb16a79bd3f3ceff3fdcdf2cb
Alec Loeber <alecloeber@yahoo.com>
2019-01-16 18:31:05 -0500

    Fixed Image Problem. Added them back to sub-menus.

--- menu/models.py
+++ menu/models.py
@@ -16,6 +16,7 @@ class Item(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
     price = models.DecimalField(max_digits=3, decimal_places=1)
+    image = models.ImageField(upload_to='food_pictures', blank=True)
 
     def __str__(self):
         return self.name
--- menu/urls.py
+++ menu/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home_page, name='home_page'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
